714bd90c6646229e5d3ae71a9ffa57be30a99044
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-04-18 13:19:25 +0530

    'create-popUp'

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React,{ FC, useState} from 'react'
 import './Footer.css'
 
-const Footer = () => {
+const Footer: FC= () => {
+    const [popUp, setPopUp] = useState<Boolean>(false)
     return (
         <div className='footer'>
-            <button>RULES</button>
+            <button onClick={model}>RULES</button>
+
+            {popUp &&
+            <div className="box">
+                <div className="model">
+                <div className="top">
+                    <h3>RULES</h3>
+                    <h3 onClick={model}>X</h3>
+                </div>
+                <div className="rules">
+                    <div className="img">
+                    </div>
+                </div>
+                </div>
+            </div>
+            }
         </div>
     )
+
+    function model():void{
+        setPopUp(prevState => !prevState)
+    }
 }
 
 export default Footer
