c2c6919121b4467a99205ae6375d306ab6e0bce7
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-04-18 15:34:12 +0530

    'create-prototype'

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,16 +1,53 @@
-import React from 'react'
+import React,{ FC,useState,useEffect} from 'react'
 import './Main.css'
+import {housePick,check} from '../../utils'
 
-const Main = () => {
+const Main:FC = () => {
+    const [select,setSelect] = useState<string>('')
+    const [com,setCom] =useState<string>('')
+    const [ans,setAns] = useState<boolean>(true)
+
+    const [result,setResult] = useState<string|undefined>('')
+
+    useEffect(() => {
+        console.log(select,com)
+        setResult(check(select, com))
+    }, [select,com])
     return (
         <div className="main">
+            {ans ?
             <div className="options">
-            <button className="rock"></button>
-            <button className="paper"></button>
-            <button className="scissors"></button>
+            <button onClick={(e) => choice(e)} className="rock"></button>
+            <button onClick={(e) => choice(e)} className="paper"></button>
+            <button onClick={(e) => choice(e)} className="scissors"></button>
             </div>
+            :<div className="check">
+                <div>
+                    <h3>YOU PICKED</h3>
+                    <button className={select}></button>
+                </div>
+                <div className="result">
+                    <h3>{result}</h3>
+                    <button onClick={() =>setAns(true)}>PLAY AGAIN</button>
+                </div>
+                <div>
+                    <h3>THE HOUSE PICKED</h3>
+                    <button className={com}></button>
+                </div>
+            </div>
+            }
         </div>
     )
+
+     function choice(e:any){
+         setSelect(e.target.className)
+         console.log("e:",e.target.className,"state:",select)
+
+         setCom(housePick(0,2))
+         console.log("fun:",housePick(0,2),"state:",com)
+
+         setAns(false)
+    }
 }
 
 export default Main
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,33 @@
 export function randomNumber(min:number,max:number){
     return Math.floor(Math.random() *(max-min +1)) +min
 }
+
+export function housePick(min:number,max:number){
+    let random = randomNumber(min,max);
+    let arr = ["rock","paper","scissors"]
+    return arr[random]
+}
+
+export function check(select:string,com:string){
+    let a = select;
+    let b = com;
+
+    if(a==='rock' && b==='paper'){
+    return 'you lose'
+    }else if(a==='paper' && b==='rock'){
+        return 'you won'
+    }else if(a==='rock' && b==='scissors'){
+        return 'you won'
+    }else if(a==='scissors' && b==='rock'){
+        return 'you lose'
+    }
+    else if(a==='paper' && b==='scissors'){
+        return 'you lose'
+    }else if(a==='scissors' && b==='paper'){
+        return 'you won'
+    }
+    else if(a === b){
+        return 'draw'
+    }
+}
+
